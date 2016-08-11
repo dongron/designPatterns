ff75422a5a52209bd78965efc3b34b576d72a51b
Dominik <dongron@wp.pl>
2016-08-12 00:39:50 +0200

    Comments added.

--- builder/src/com/company/Main.java
+++ builder/src/com/company/Main.java
@@ -8,10 +8,11 @@ import com.company.builder.entities.Beer;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // Fluent Builder DP
         BeerModern beer01 = new BeerModern.Builder().name("Rowing Jack").style("AIPA").ibu(120).build();
         System.out.println(beer01.toString());
 
+        // Builder DP
         BeerBuilder rowingJackBuilder = new RowingJackBuilder();
         BeerDirector beerDirector = new BeerDirector(rowingJackBuilder);
         beerDirector.makeBeer();
